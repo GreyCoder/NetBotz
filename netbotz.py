5cae509b67962df8941ac77c52a0e9025556b8c1
unknown <ic1002@TAG313450.matrix.txstate.edu>
2012-06-02 05:32:12 -0500

    Add:  Added refresh methods and separated initialize also changed variable names to make it more readable

--- netbotz.py
+++ netbotz.py
@@ -38,7 +38,7 @@ def nb_report(ip, uname, pw):
 
 
 class NetBotz(object):
-    """Netbotz Class requires IP, Username, Password"""
+    """Netbotz Class requires IP, Username, Password to create"""
     def __init__(self, ip, username, password):
         self.url = 'https://%s/cgi-bin/nbSensorWebServices?wsdl' % ip
         self.location = 'https://%s/cgi-bin/nbSensorWebServices' % ip
@@ -48,39 +48,79 @@ class NetBotz(object):
         self.sensors = {}
 
     def initialize_environment(self):
+        """Initialize environment variables for this NetBotz object"""
         self.sensors['environment'] = {}
         for pod in self.client.service.getAllPodIDs()[0]:
-            self.podLocation = self.client.service.getPod(pod)['Label']
             if len(self.client.service.getAllNumSensorIDsForPod(pod)) != 0:
-                self.sensors['environment'][self.podLocation] = {}
-                for sensorndx in self.client.service.getAllNumSensorIDsForPod(pod)[0]:
-                    self.sensors['environment'][self.podLocation][sensorndx] = self.client.service.getNumSensor(sensorndx)['Value']
+                self.sensors['environment'][pod] = {}
+                self.sensors['environment'][pod]['label'] = self.client.service.getPod(pod)['Label']
+                for sensor in self.client.service.getAllNumSensorIDsForPod(pod)[0]:
+                    self.sensors['environment'][pod][sensor] = self.client.service.getNumSensor(sensor)['Value']
 
     def initialize_state(self):
+        """Initialize state variables for this NetBotz object"""
         self.sensors['state'] = {}
         for pod in self.client.service.getAllPodIDs()[0]:
             self.podLocation = self.client.service.getPod(pod)['Label']
             if len(self.client.service.getAllStateSensorIDsForPod(pod)) != 0:
-                self.sensors['state'][self.podLocation] = {}
-                for sensorndx in self.client.service.getAllStateSensorIDsForPod(pod)[0]:
-                    self.sensors['state'][self.podLocation][sensorndx] = self.client.service.getStateSensor(sensorndx)['ValueIndex']
+                self.sensors['state'][pod] = {}
+                self.sensors['state'][pod]['label'] = self.client.service.getPod(pod)['Label']
+                for sensor in self.client.service.getAllStateSensorIDsForPod(pod)[0]:
+                    self.sensors['state'][pod][sensor] = self.client.service.getStateSensor(sensor)['ValueIndex']
 
-    def initialize(self):
+    def initialize_all(self):
+        """Initialize all variables for this NetBotz object"""
         self.initialize_state()
         self.initialize_state()
 
-    def nb_report(self):
-        for pod in self.client.service.getAllPodIDs()[0]:
-            podLocation = self.client.service.getPod(pod)['Label']
+    def initialize(self, sensortype='all'):
+        if (sensortype == 'environment' or sensortype == 'e'):
+            self.initialize_environment()
+        elif (sensortype == 'state' or sensortype =='s'):
+            self.initialize_state()
+        elif sensortype == 'all':
+            self.initialize_all()
+        else:
+            print "\nUnknown Sensor Type\n"
 
-            print "\n" + podLocation
-            if len(self.client.service.getAllNumSensorIDsForPod(pod)) != 0:
-                print "    Environment Sensors"
-                for sensorndx in self.client.service.getAllNumSensorIDsForPod(pod)[0]:
-                    print "        " + sensorndx + " = " + \
-                    str(self.client.service.getNumSensor(sensorndx)['Value'])
-            if len(self.client.service.getAllStateSensorIDsForPod(pod)) != 0:
-                print "    State Sensors"
-                for sensorndx in self.client.service.getAllStateSensorIDsForPod(pod)[0]:
-                    print "        " + sensorndx + " = " + \
-                    str(self.client.service.getStateSensor(sensorndx)['ValueIndex'])
+    def refresh(self, sensortype,verbose=False):
+        if sensortype == 'environment':
+            for pod in self.sensors['environment']:
+                self.sensors['environment'][pod]['label'] = self.client.service.getPod(pod)['Label']
+                for sensor in self.sensors['environment'][pod]:
+                    if sensor != 'label':
+                        if type(self.client.service.getNumSensor(sensor)['Value']) is not None:
+                            self.sensors['environment'][pod][sensor] = self.client.service.getNumSensor(sensor)['Value']
+                            if verbose == True:
+                                print 'updated  ' + sensor
+                        else:
+                            print sensor + ' is None\n'
+        if sensortype == 'state':
+            for pod in self.sensors['state']:
+                self.sensors['state'][pod]['label'] = self.client.service.getPod(pod)['Label']
+                for sensor in self.sensors['state'][pod]:
+                    if sensor != 'label':
+                        if type(self.client.service.getStateSensor(sensor)['ValueIndex']) is not None:
+                            self.sensors['state'][pod][sensor] = self.client.service.getStateSensor(sensor)['ValueIndex']
+                            if verbose == True:
+                                print 'updated  ' + sensor
+                        else:
+                            print sensor + ' is None\n'
+
+    def report(self, sensortype='all'):
+        if (sensortype == 'environment' or sensortype == 'e' or sensortype =='all'):
+            if 'environment' in self.sensors:
+                print "Environment Sensors"
+                for pod in self.sensors['environment']:
+                    print '    ' + self.sensors['environment'][pod]['label']
+                    for sensor in self.sensors['environment'][pod]:
+                        if sensor  != 'label':
+                            print "        " + sensor + " = " + str(self.sensors['environment'][pod][sensor])
+        if (sensortype == 'state' or sensortype == 's' or sensortype =='all'):
+            if 'state' in self.sensors:
+                print "State Sensors"
+                for pod in self.sensors['state']:
+                    print '    ' + self.sensors['state'][pod]['label']
+                    for sensor in self.sensors['state'][pod]:
+                        if sensor  != 'label':
+                            print "        " + sensor + " = " + str(self.sensors['state'][pod][sensor])
